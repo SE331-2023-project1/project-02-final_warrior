5dea3ea87f0fe89fe32cc0b875459e4e45dea087
Thai <thaiphat_s@cmu.ac.th>
2023-10-15 13:38:26 +0700

    Update StudentController
    Add getter function

--- rest/src/main/java/se331/proj/rest/controller/StudentController.java
+++ rest/src/main/java/se331/proj/rest/controller/StudentController.java
@@ -1,6 +1,10 @@
 package se331.proj.rest.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import se331.proj.rest.entity.Student;
 
 import jakarta.annotation.PostConstruct;
@@ -23,4 +27,18 @@ public class StudentController {
         .imageLink("https://i.redd.it/jn8p6oejm0ub1.jpg")
         .build());
     }
+
+    
+    @GetMapping("students")
+    public ResponseEntity<?> getStudentLists(@RequestParam(value = "_limit", required = false) Integer perPage,
+        @RequestParam(value = "_page", required = false) Integer page) {
+            perPage = perPage == null?studentList.size():perPage;
+            page = page == null?1:page;
+            Integer firstIndex = (page-1) * perPage;
+            List<Student> output = new ArrayList<>();
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(studentList.get(i));
+            }
+            return ResponseEntity.ok(output);
+        }
 }
