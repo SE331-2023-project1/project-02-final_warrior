9ff4c2f8b8baf12b9123672a87ed5380009fcb29
Thai <thaiphat_s@cmu.ac.th>
2023-10-15 14:33:09 +0700

    Update application.java
    add mvc config

--- rest/src/main/java/se331/proj/rest/Application.java
+++ rest/src/main/java/se331/proj/rest/Application.java
@@ -2,6 +2,9 @@ package se331.proj.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,26 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// students
+				registry.addMapping("/students")
+				.allowedOrigins("https://localhost:3000");
+				registry.addMapping("/students/*")
+				.allowedOrigins("https://localhost:3000");
+				// advisors
+				registry.addMapping("/advisors")
+				.allowedOrigins("https://localhost:3000");
+				registry.addMapping("/advisors/*")
+				.allowedOrigins("https://localhost:3000");
+				// **
+				registry.addMapping("/**")
+				.allowedOrigins("https://localhost:3000")
+				.exposedHeaders("x-total-count");
+			}
+		};
+	}
 }
