f45898406f68a860bde3989ae71267f36e7de790
Thai <thaiphat_s@cmu.ac.th>
2023-10-15 13:20:47 +0700

    Update StudentController

--- rest/src/main/java/se331/proj/rest/controller/StudentController.java
+++ rest/src/main/java/se331/proj/rest/controller/StudentController.java
@@ -14,6 +14,13 @@ public class StudentController {
     @PostConstruct
     public void init() {
         studentList = new ArrayList<>();
-        studentList.add(Student.builder().build());
+        studentList.add(Student.builder()
+        .id(642115024)
+        .name("Thaipat")
+        .surname("Sukhumpraisan")
+        .dept("CAMT")
+        .advisorId(1234)
+        .imageLink("https://i.redd.it/jn8p6oejm0ub1.jpg")
+        .build());
     }
 }
