7dff119a7d38518aa584412f1363211abd42dcd7
Thai <thaiphat_s@cmu.ac.th>
2023-10-16 19:42:39 +0700

    Create initapp config

---
+++ rest/src/main/java/se331/proj/rest/config/InitApp.java
@@ -0,0 +1,43 @@
+package se331.proj.rest.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import se331.proj.rest.entity.Advisor;
+import se331.proj.rest.entity.Student;
+import se331.proj.rest.repository.AdvisorRepository;
+import se331.proj.rest.repository.StudentRepository;
+
+@Component
+@RequiredArgsConstructor
+public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
+    final StudentRepository studentRepository;
+    final AdvisorRepository advisorRepository;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        studentRepository.save(Student.builder()
+            .id(642115025)
+            .name("Thaipat2")
+            .surname("Sukhumpraisan2")
+            .dept("CAMT2")
+            .advisorId(1234)
+            .imageLink("https://i.redd.it/jn8p6oejm0ub1.jpg")
+            .build());
+
+        advisorRepository.save(Advisor.builder()
+        .id(1234)
+        .name("John")
+        .surname("Doe")
+        .dept("CAMT")
+        .position("Lecturer")
+        .studentId(new ArrayList<Integer>(Arrays.asList(642115024)))
+        .imageLink("https://i.redd.it/qjfd7hi1w8ub1.jpg")
+        .build());
+    }
+}
