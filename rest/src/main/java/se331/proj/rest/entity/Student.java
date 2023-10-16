ea073e167ce361f9f4b153046080516b59f4dcf4
Thai <thaiphat_s@cmu.ac.th>
2023-10-16 19:41:23 +0700

    Update entity

--- rest/src/main/java/se331/proj/rest/entity/Student.java
+++ rest/src/main/java/se331/proj/rest/entity/Student.java
@@ -1,11 +1,24 @@
 package se331.proj.rest.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
     Integer id;
     String name;
     String surname;
