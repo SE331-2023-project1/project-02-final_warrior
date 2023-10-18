edc6ca45a2e094a2188ff7cc0f199aeeea471a75
Thai <thaiphat_s@cmu.ac.th>
2023-10-18 18:15:39 +0700

    Update repository

--- rest/src/main/java/se331/proj/rest/repository/AdvisorRepository.java
+++ rest/src/main/java/se331/proj/rest/repository/AdvisorRepository.java
@@ -2,10 +2,10 @@ package se331.proj.rest.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import se331.proj.rest.entity.Advisor;
 
-public interface AdvisorRepository extends CrudRepository<Advisor,Integer> {
+public interface AdvisorRepository extends JpaRepository<Advisor,Integer> {
     List<Advisor> findAll();
 }
--- rest/src/main/java/se331/proj/rest/repository/StudentRepository.java
+++ rest/src/main/java/se331/proj/rest/repository/StudentRepository.java
@@ -2,10 +2,10 @@ package se331.proj.rest.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import se331.proj.rest.entity.Student;
 
-public interface StudentRepository extends CrudRepository<Student,Integer>{
+public interface StudentRepository extends JpaRepository<Student,Integer>{
     List<Student> findAll();
 }
