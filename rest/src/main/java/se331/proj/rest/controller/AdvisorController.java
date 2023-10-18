7de8d77f067ef356d6ba8d124249bd2f5bc84bbd
Thai <thaiphat_s@cmu.ac.th>
2023-10-18 18:16:05 +0700

    Update controller

--- rest/src/main/java/se331/proj/rest/controller/AdvisorController.java
+++ rest/src/main/java/se331/proj/rest/controller/AdvisorController.java
@@ -1,19 +1,20 @@
 package se331.proj.rest.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
 import se331.proj.rest.entity.Advisor;
 import se331.proj.rest.service.AdvisorService;
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,18 +24,12 @@ public class AdvisorController {
     @GetMapping("advisors")
     public ResponseEntity<?> getAdvisorLists(@RequestParam(value = "_limit", required = false) Integer perPage,
         @RequestParam(value = "_page", required = false) Integer page) {
-            List<Advisor> output = null;
-            Integer advisorSize = advisorService.getAdvisorSize();
+            Page<Advisor> pageOutput = advisorService.getAdvisors(perPage, page);
 
             HttpHeaders responseHeader = new HttpHeaders();
-            responseHeader.set("x-total-count", String.valueOf(advisorSize));
 
-            try {
-                output = advisorService.getAdvisors(perPage, page);
-                return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-            } catch (IndexOutOfBoundsException ex) {
-                return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-            }
+            responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+            return new ResponseEntity<>(pageOutput.getContent(), responseHeader, HttpStatus.OK);
     }
 
     
@@ -46,5 +41,11 @@ public class AdvisorController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
+    }
+
+    @PostMapping("/advisors")
+    public ResponseEntity<?> addAdvisor(@RequestBody Advisor advisor) {
+        Advisor output = advisorService.save(advisor);
+        return ResponseEntity.ok(output);
     }
 }
--- rest/src/main/java/se331/proj/rest/controller/StudentController.java
+++ rest/src/main/java/se331/proj/rest/controller/StudentController.java
@@ -1,19 +1,20 @@
 package se331.proj.rest.controller;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.server.ResponseStatusException;
 
+import lombok.RequiredArgsConstructor;
 import se331.proj.rest.entity.Student;
 import se331.proj.rest.service.StudentService;
-import lombok.RequiredArgsConstructor;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,19 +24,12 @@ public class StudentController {
     @GetMapping("students")
     public ResponseEntity<?> getStudentLists(@RequestParam(value = "_limit", required = false) Integer perPage,
         @RequestParam(value = "_page", required = false) Integer page) {
-             List<Student> output = null;
-            Integer studentSize = studentService.getStudentSize();
+            Page<Student> pageOutput = studentService.getStudents(perPage, page);
 
             HttpHeaders responseHeader = new HttpHeaders();
-            responseHeader.set("x-total-count", String.valueOf(studentSize));
 
-            try {
-                output = studentService.getStudents(perPage, page);
-                return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-            } catch (IndexOutOfBoundsException ex) {
-                return new ResponseEntity<>(output, responseHeader, HttpStatus.OK);
-            }
-    
+            responseHeader.set("x-total-count", String.valueOf(pageOutput.getTotalElements()));
+            return new ResponseEntity<>(pageOutput.getContent(), responseHeader, HttpStatus.OK);
     }
 
     @GetMapping("students/{id}")
@@ -46,5 +40,11 @@ public class StudentController {
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
         }
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<?> addStudent(@RequestBody Student student) {
+        Student output = studentService.save(student);
+        return ResponseEntity.ok(output);
     }
 }
