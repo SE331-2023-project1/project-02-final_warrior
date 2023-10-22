3103b8c87849120b8a735a8d0a8be4bbacf8d5e5
Taninwat <fame5454@gmail.com>
2023-10-22 08:54:29 +0700

    Add security

---
+++ rest/src/main/java/se331/proj/rest/security/auth/AuthenticationResponse.java
@@ -0,0 +1,28 @@
+package se331.proj.rest.security.auth;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+//import se331.proj.rest.entity.Organizer;
+//import se331.proj.rest.entity.OrganizerAuthDTO;
+//import se331.proj.rest.entity.OrganizerDTO;
+import se331.proj.rest.security.user.Role;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+  @JsonProperty("access_token")
+  private String accessToken;
+  @JsonProperty("refresh_token")
+  private String refreshToken;
+  @JsonProperty("user_role")
+  private List<Role> userRole;
+//  private OrganizerAuthDTO user;
+}
---
+++ rest/src/main/java/se331/proj/rest/security/user/Role.java
@@ -0,0 +1,6 @@
+package se331.proj.rest.security.user;
+
+public enum Role {
+
+  ROLE_FASTFIT, ROLE_STUDENT, ROLE_ADMIN, ROLE_SUPER_ADMIN, ROLE_ADVISOR
+}
