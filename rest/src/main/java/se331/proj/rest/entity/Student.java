c7bd114b42d18b685e61fd012d9d39ca43958a45
Thai <thaiphat_s@cmu.ac.th>
2023-10-23 20:03:22 +0700

    Revert "Update entity"
    This reverts commit e50be8e5c95c9323858bd52e848a50859aa5f38b.

--- rest/src/main/java/se331/proj/rest/entity/Student.java
+++ rest/src/main/java/se331/proj/rest/entity/Student.java
@@ -8,13 +8,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import se331.proj.rest.security.user.User;
 
 @Data
 @Builder
@@ -34,6 +32,4 @@ public class Student {
     String dept;
     @ManyToOne
     Advisor advisor;
-    @OneToOne
-    User user;
 }
